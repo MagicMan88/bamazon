cf9c415d8d5f4a647008e6e723fc02fe4173c337
Stephen Green <stephengreen1028@gmail.com>
2020-01-11 14:33:33 -0700

    Added logic to the first function, displayProd

--- bamazonCus.js
+++ bamazonCus.js
@@ -3,7 +3,7 @@ var inquirer = require('inquirer');
 var mysql = require('mysql');
 var Table = require('cli-table');
 
-// Connect to the appropriate database using connection.connect
+// Connect to the appropriate database
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -16,4 +16,18 @@ connection.connect(function(err) {
     if(err) throw err;
     console.log("Connected as id " + connection.threadId);
 });
+
+// Function to display the available products
+var displayProd = function() {
+    var query = 'SELECT * FROM products';
+    connection.query(query, function(err, res) {
+        if(err) throw err;
+        // Display the table using the cli-table package
+        var displayTable = new Table ({
+            head: ['Item ID', 'Product Name', 'Category', 'Price', 'Quantity'],
+            colWidths: [10,25,25,10,14]
+        });
+        // Loop through the results using a for loop
+    })
+}
 
