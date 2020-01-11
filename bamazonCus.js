8e0feaba25ce45b166540f620cdc9be2a34b2da9
Stephen Green <stephengreen1028@gmail.com>
2020-01-11 14:29:16 -0700

    connected to the bamazon database

--- bamazonCus.js
+++ bamazonCus.js
@@ -4,3 +4,16 @@ var mysql = require('mysql');
 var Table = require('cli-table');
 
 // Connect to the appropriate database using connection.connect
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Ilovetolearn!',
+    database: 'bamazon'
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected as id " + connection.threadId);
+});
+
